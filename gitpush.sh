9794bccd24bfb134d71e409f23977fbdf678017e
SerCom_KC <szescxz@gmail.com>
2018-06-06 03:17:13 +0800

    Add travis-ci script

---
+++ gitpush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd _site/ppgd
+if [ -n "$(git status --porcelain)" ]; then
+    echo -e \\033[32mChanges detected, configuring git\\033[0m
+    git config user.name 'Translated by @SerCom_KC'
+    git config user.email 'sckctranslate@sercomkc.org'
+    git config push.default simple
+    echo -e \\033[32mGit configuration completed. Committing changes now\\033[0m
+    git add -A
+    git commit -m 'Site update'
+    echo -e \\033[32mChanges committed, pushing\\033[0m
+    git push
+fi
